c6dc4bd409341adc866faae9c0760fa09c50dc1e
Jeremy Dsilva <jeremydsilva7@gmail.com>
2020-12-08 03:03:45 +0400

    Added leaderCount Usecase

---
+++ src/main/java/dto/LeaderCount.java
@@ -0,0 +1,24 @@
+package dto;
+
+public class LeaderCount {
+
+    Long teamLeaderCount;
+    Long peerLeaderCount;
+
+    public Long getTeamLeaderCount() {
+        return teamLeaderCount;
+    }
+
+    public void setTeamLeaderCount(Long teamLeaderCount) {
+        this.teamLeaderCount = teamLeaderCount;
+    }
+
+    public Long getPeerLeaderCount() {
+        return peerLeaderCount;
+    }
+
+    public void setPeerLeaderCount(Long peerLeaderCount) {
+        this.peerLeaderCount = peerLeaderCount;
+    }
+    
+}
