dd632c9d002fc836d46984bc76fefd522cf01c54
Jeremy Dsilva <jeremydsilva7@gmail.com>
2020-12-07 17:28:34 +0400

    Added Dictionary

--- src/main/java/app/AppContext.java
+++ src/main/java/app/AppContext.java
@@ -1,17 +1,31 @@
 package app;
 
+import java.util.Dictionary;
+
 import entity.User;
 
 public class AppContext {
 
     static User user;
 
-    public static User getUser(){
+    static Long groupId = null;
+
+    static Dictionary<String, Object> dictionary;
+
+    public static User getUser() {
         return AppContext.user;
     }
 
-    public static void setUser(User user){
+    public static void setUser(User user) {
         AppContext.user = user;
     }
-    
+
+    public static Object get(String value) {
+        return AppContext.dictionary.get(value);
+    }
+
+    public static void put(String value, Object obj) {
+        AppContext.dictionary.put(value, obj);
+    }
+
 }
